f809b3f6e7c4869e4a959a28aad114abfc052bd2
Vadim Vyalov <vyalov.vadim@gmail.com>
2023-01-04 05:25:59 +0200

    first

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,38 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+const THROTTLE_TIME=1000; // ms
+const LS_KEY_PLAYER_TIME="videoplayer-current-time"
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const saveLS = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set LS state error: ", error.message);
+  }
+};
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const loadLS = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get LS state error: ", error.message);
+  }
+};
+
+
+const saveTimePosition=(data)=>{
+  saveLS(LS_KEY_PLAYER_TIME,data);
+  }
+  
+const curentPositionPlay=loadLS(LS_KEY_PLAYER_TIME);
+
+if (curentPositionPlay) {
+  player.setCurrentTime(curentPositionPlay.seconds);
+}
+
+player.on('timeupdate', throttle(saveTimePosition,THROTTLE_TIME));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,67 @@
+import throttle from 'lodash.throttle';
+
+const formFB = document.querySelector('.feedback-form');
+
+const THROTTLE_TIME=500; // ms
+const LS_KEY_FEEDBACK_FORM="feedback-form-state"
+
+// function >>>>>
+const saveLS = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set LS state error: ", error.message);
+  }
+};
+
+const loadLS = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get LS state error: ", error.message);
+  }
+};
+
+const saveFeedback = (evt) =>{
+    const formData=new FormData(formFB); 
+
+    //! const formData=new FormData(evt.currentTarget); 
+    //! evt.currentTargen працює нестабільно  разом з throttle  
+    
+    // let data = {};
+    // formData.forEach((value, key) => data[key] = value);
+    // saveLS(LS_KEY_FEEDBACK_FORM, data);
+
+     saveLS(LS_KEY_FEEDBACK_FORM, Object.fromEntries(formData));
+    
+};
+
+const clearFeedback = (evt) =>{
+    evt.preventDefault();
+  
+    localStorage.removeItem(LS_KEY_FEEDBACK_FORM);
+  
+    const data=new FormData(evt.currentTarget); 
+    console.log(Object.fromEntries(data));
+    evt.currentTarget.reset();
+  };
+
+  const updateForm=() =>{
+    const data=loadLS(LS_KEY_FEEDBACK_FORM);
+    //console.log(data);
+    if (data) {
+        formFB.elements.message.value=data.message;
+        formFB.elements.email.value=data.email;
+    }
+    };
+
+// main >>>>>>>>
+
+updateForm();
+formFB.addEventListener('input',throttle(saveFeedback,THROTTLE_TIME));
+// formFB.addEventListener('input',saveFeedback);
+formFB.addEventListener('submit', throttle(clearFeedback,THROTTLE_TIME));
+
 
